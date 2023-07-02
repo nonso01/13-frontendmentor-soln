2585d499bcde6f4a271bd00da08e1ad612943728
nonso01 <chukwunonsomartin01@gmail.com>
2023-07-02 20:13:23 +0100

    remove eruda

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./scss/main.css"
 
-import eruda from "eruda"
+//import eruda from "eruda"
 
-eruda.init()
+//eruda.init()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
